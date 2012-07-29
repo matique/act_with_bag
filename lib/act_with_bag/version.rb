245fdb8ea8b33402a41d93304533a6ca4b4394bc
matique <dittmar.krall@matique.de>
2012-07-29 08:38:46 +0200

    '...'

--- lib/act_with_bag/version.rb
+++ lib/act_with_bag/version.rb
@@ -1,3 +1,3 @@
 module ActWithBag
-  VERSION = "0.3.0"
+  VERSION = "0.4.0"
 end
---
+++ test/delete_test.rb
@@ -0,0 +1,26 @@
+require 'test_helper'
+
+
+class Order < ActiveRecord::Base
+  add_to_bag :aa, :bb, :cc
+  delete_from_bag :bb
+end
+
+
+class DeleteTest < ActiveSupport::TestCase
+
+  def setup
+    @order = Order.new
+  end
+
+  test "delete a field" do
+    @order.aa = 'aa'
+    @order.bb = 'bb'
+    @order.cc = 'cc'
+    @order.save
+    assert_equal 'aa', @order.aa
+    assert_nil @order.bb
+    assert_equal 'cc', @order.cc
+  end
+
+end
