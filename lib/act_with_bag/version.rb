94e9dbe634ca71c6b9cbaf62fbffb54d58c12a79
matique <dittmar.krall@matique.de>
2019-09-27 11:51:14 +0200

    'v1.0.0'

--- lib/act_with_bag/version.rb
+++ lib/act_with_bag/version.rb
@@ -1,6 +1,6 @@
 module ActWithBag
-#  VERSION = '1.0.0'     # 2019-09-27
-  VERSION = '0.5.7'     # 2019-03-05
+  VERSION = '1.0.0'     # 2019-09-27
+#  VERSION = '0.5.7'     # 2019-03-05
 #  VERSION = '0.5.6'     # 2019-02-15
 #  VERSION = '0.5.5'
 end
