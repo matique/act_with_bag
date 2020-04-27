ae02854e044377086abd999adbbbd3f0bba57c43
matique <dittmar.krall@matique.de>
2020-04-27 12:25:05 +0200

    'rubocop; v1.0.1'

--- lib/act_with_bag.rb
+++ lib/act_with_bag.rb
@@ -1,90 +1,94 @@
-class << ActiveRecord::Base
+# frozen_string_literal: true
 
+class << ActiveRecord::Base
   def add_to_bag(*baglets)
-#p "baglets #{baglets.inspect}"
+    # p "baglets #{baglets.inspect}"
     serialize :bag, Hash
 
-    self.class_eval %{
+    class_eval %{
       def bag=(x)
-	#bag changes disabled as it must be handled by Bag himself
+        #bag changes disabled as it must be handled by Bag himself
       end
     }
 
     @baggies_date ||= {}
-    baglets.each {|b|
+    baglets.each { |b|
       if b.is_a?(Hash)
-	b.each {|baggie, type|
-	  add_accessor(baggie, type)
-	}
+        b.each { |baggie, type|
+          add_accessor(baggie, type)
+        }
       else
-	add_accessor(b, :field)
+        add_accessor(b, :field)
       end
     }
   end
 
   def delete_from_bag(*baglets)
-#p "delete_from_bag baglets #{baglets.inspect}"
+    # p "delete_from_bag baglets #{baglets.inspect}"
 
-    self.class_eval %{
+    class_eval %{
       before_save do
-	#{baglets}.each {|b|
-	  if b.is_a?(Hash)
-	    b.each {|baggie, type|
-	      self.bag.delete(baggie.to_sym)
-	    }
-	  else
-	    self.bag.delete(b.to_sym)
-	  end
-	}
+        #{baglets}.each {|b|
+          if b.is_a?(Hash)
+            b.each { |baggie, type|
+              self.bag.delete(baggie.to_sym)
+            }
+          else
+            self.bag.delete(b.to_sym)
+          end
+        }
       end
     }
-
   end
 
-  def merge(bag, params)
-    model = self.to_s.underscore.to_sym
+  def merge(_bag, params)
+    model = to_s.underscore.to_sym
     return params unless params[model]
 
-    (@baggies_date || {}).each {|baggie, type|
-      res, found, stopped = [], false, false
-      (1..3).each {|i|
-	p = params[model].delete("#{baggie}(#{i}i)")
-	break if p.nil?
-	found = true
-	stopped = true if p.empty?
-	res << p.to_i unless stopped
+    (@baggies_date || {}).each { |baggie, _type|
+      res = []
+      found = false
+      stopped = false
+      (1..3).each { |i|
+        p = params[model].delete("#{baggie}(#{i}i)")
+        break if p.nil?
+
+        found = true
+        stopped = true if p.empty?
+        res << p.to_i unless stopped
       }
       next unless found
-  ## weird Timestamp, Hash and YAML problem
+
+      ## weird Timestamp, Hash and YAML problem
       res = [0]  if res == []
       res[0] = 0  unless res[0] >= 0
 
-      value = Date.new(*res) rescue value = nil
+      value = Date.new(*res) rescue nil
       params[model][baggie] = value
     }
     params
   end
-
 
  protected
   def accessor_present?(accessor)
     accessor_sym = accessor.to_sym
     res = false
 
-    res = true  if self.method_defined?(accessor_sym)
-    res = true  if self.respond_to?(:attribute_names) &&
-		   self.attribute_names.include?(accessor)
+    res = true  if method_defined?(accessor_sym)
+    res = true  if respond_to?(:attribute_names) &&
+                   attribute_names.include?(accessor)
     if res
-#      logger.info "** Already defined #{self.to_s}.#{accessor}"
-#p "** act_to_bag: untouched accessor '#{self.to_s}.#{accessor}'"
+      #      logger.info "** Already defined #{self.to_s}.#{accessor}"
+      # p "** act_to_bag: untouched accessor '#{self.to_s}.#{accessor}'"
     end
-    return res
+    res
   end
 
   def add_accessor(baggie, type)
     accessor = baggie.to_s
     return  if accessor_present?(accessor)
-#MARS patch, new:
+
+    # MARS patch, new:
     #
     # The @baggies field is indispensable. Otherwise mistakes in renaming
     # or deleting baggies (without renaming/deleting) the value entry from the
@@ -94,69 +98,66 @@ class << ActiveRecord::Base
     # doing completely other things (e. g. accessing a database column).
     #
     @baggies ||= {}
-    baggies = ( @baggies[self.to_s.underscore.to_sym] ||= {} )
-    baggies[ baggie ] = type
-#END
+    baggies = (@baggies[to_s.underscore.to_sym] ||= {})
+    baggies[baggie] = type
+    # END
 
     type_sym = type.to_sym
     typing = {integer: '.to_i', float: '.to_f',
-	      string: '.to_s'}[type_sym] || ''
-#p "add_accessor #{self.to_s} #{baggie.inspect} #{type_sym.inspect}"
+              string: '.to_s'}[type_sym] || ''
+    # p "add_accessor #{self.to_s} #{baggie.inspect} #{type_sym.inspect}"
 
     @baggies_date[baggie] = type  if type_sym == :date
 
-    unless type_sym == :boolean
-      self.class_eval %{
-	def #{accessor}
-	  self.bag && self.bag[:#{baggie}]
-	end
-      }
+    if type_sym == :boolean
+      class_eval %(
+        def #{accessor}
+          res = bag && bag[:#{baggie}]
+          return res if res.class == FalseClass
+          return res if res.class == TrueClass
+          return res.to_i != 0
+          res
+        end
+      )
 
-      self.class_eval %{
-	def #{accessor}=(value)
-	  @attributes['bag'] = {}  unless bag.is_a?(Hash)
-	  unless value.nil?
-	    self.bag[:#{baggie}] = value#{typing}
-	  else
-	    self.bag.delete(:#{baggie})
-	    nil
-	  end
-	end
-      }
+      class_eval %(
+        def #{accessor}=(value)
+          @attributes['bag'] = {}  unless bag.is_a?(Hash)
+          falsys = [false, "false", 0, "0", nil]
+          unless falsys.include?(value)
+            self.bag[:#{baggie}] = (value == 'true') || value
+          else
+            self.bag.delete(:#{baggie})
+            nil
+          end
+        end
+      )
 
-    else   ################ now boolean handling ###################
+      class_eval %(
+        def #{accessor}?
+          #{accessor}
+        end
+      )
 
-      self.class_eval %{
-	def #{accessor}
-	  res = bag && bag[:#{baggie}]
-	  return res if res.class == FalseClass
-	  return res if res.class == TrueClass
-	  return res.to_i != 0
-	  res
-	end
-      }
+    else
 
-      self.class_eval %{
-	def #{accessor}=(value)
-	  @attributes['bag'] = {}  unless bag.is_a?(Hash)
-	  falsys = [false, "false", 0, "0", nil]
-	  unless falsys.include?(value)
-	    self.bag[:#{baggie}] = (value == 'true') || value
-	  else
-	    self.bag.delete(:#{baggie})
-	    nil
-	  end
-	end
-      }
+      class_eval %(
+        def #{accessor}
+          self.bag && self.bag[:#{baggie}]
+        end
+      )
 
-      self.class_eval %{
-	def #{accessor}?
-	  #{accessor}
-	end
-      }
-
+      class_eval %(
+        def #{accessor}=(value)
+          @attributes['bag'] = {}  unless bag.is_a?(Hash)
+          unless value.nil?
+            self.bag[:#{baggie}] = value#{typing}
+          else
+            self.bag.delete(:#{baggie})
+            nil
+          end
+        end
+      )
     end
-
   end
-
 end
--- lib/act_with_bag/version.rb
+++ lib/act_with_bag/version.rb
@@ -1,6 +1,9 @@
+# rubocop: disable all
+
 module ActWithBag
-  VERSION = '1.0.0'     # 2019-09-27
-#  VERSION = '0.5.7'     # 2019-03-05
-#  VERSION = '0.5.6'     # 2019-02-15
-#  VERSION = '0.5.5'
+  VERSION = '1.0.1'     # 2020-04-27
+# VERSION = '1.0.0'     # 2019-09-27
+# VERSION = '0.5.7'     # 2019-03-05
+# VERSION = '0.5.6'     # 2019-02-15
+# VERSION = '0.5.5'
 end
