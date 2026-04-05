6bd464434949a2226f73c8c53b47485b9557df4b
dittmar <dittmar.krall@matique.de>
2026-04-05 17:39:20 +0200

    'v1.1.9'

--- lib/act_with_bag/version.rb
+++ lib/act_with_bag/version.rb
@@ -1,5 +1,6 @@
 module ActWithBag
-  VERSION = "1.1.8" # 2026-01-01
+  VERSION = "1.1.9" # 2026-04-05
+  # VERSION = "1.1.8" # 2026-01-01
   # VERSION = "1.1.7" # 2025-10-30
   # VERSION = "1.1.6" # 2025-06-17
   # VERSION = "1.1.5" # 2024-10-19
